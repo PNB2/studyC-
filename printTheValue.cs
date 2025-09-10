75621dce65d7d9f7a4749fb7edee77aa79d2ee1a
Ngọc Bội <ngocboi275@gmail.com>
2025-09-10 23:20:31 +0700

    Create printTheValue.cs
    In addition to using the method Console.WriteLine() to display information on the screen in the form of characters, you can also use this command to display the values ​​of calculations

---
+++ printTheValue.cs
@@ -0,0 +1,13 @@
+// Basic C# Exercise: Print the value of a calculation
+using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(8 + 4);
+            Console.WriteLine(8 - 4);
+            Console.WriteLine(8 * 4);
+            Console.WriteLine(8 / 4);
+        }
+    }
+}
